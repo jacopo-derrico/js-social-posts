2114611f5db3266b815d05fc13c696500258fd56
jacopo-derrico <derricoj.design@gmail.com>
2024-01-24 10:55:54 +0100

    corrected like button

--- main.js
+++ main.js
@@ -115,165 +115,22 @@ posts.forEach((element) => {
 })
 
 const likeBtn = document.querySelectorAll('.js-like-button');
-
+const counterLikes = document.querySelectorAll('.js-likes-counter');
 const likedPosts = [];
 
-likeBtn.forEach(element => {
-    element.addEventListener('click', function() {
+likeBtn.forEach((element, index) => {
+    element.addEventListener('click', function (e) {
 
-        event.preventDefault();
+        e.preventDefault();
 
-        const postID = Number(this.dataset.postid);
-        const post = posts.find(post => post.id === postID);
-        
-        const isLiked = !post.liked
-        post.liked = isLiked
+        element.classList.toggle('like-button--liked')
 
-        const likeIcon = this.querySelector('.like-button__icon');
-        const likeText = this.querySelector('.like-button__label');
-        const likeCounter = document.getElementById(`like-counter-${postID}`)
-
-        if (isLiked) {
-            likeIcon.classList.add('like-button--liked')
-            likeText.classList.add('like-button--liked')
-            post.likes++
-            likedPosts.push(postID)
-            console.log(likedPosts)
+        if (element.classList.contains('like-button--liked')) {
+            posts[index].likes++
+            counterLikes[index].innerHTML = posts[index].likes
         } else {
-            likeIcon.classList.remove('like-button--liked')
-            likeText.classList.remove('like-button--liked')
-            post.likes--
-            const posts = [
-    {
-        "id": 1,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/300?image=171",
-        "author": {
-            "name": "Phil Mangione",
-            "image": "https://unsplash.it/300/300?image=15"
-        },
-        "likes": 80,
-        "created": "2021-06-25"
-    },
-    {
-        "id": 2,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=112",
-        "author": {
-            "name": "Sofia Perlari",
-            "image": "https://unsplash.it/300/300?image=10"
-        },
-        "likes": 120,
-        "created": "2021-09-03"
-    },
-    {
-        "id": 3,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=234",
-        "author": {
-            "name": "Chiara Passaro",
-            "image": "https://unsplash.it/300/300?image=20"
-        },
-        "likes": 78,
-        "created": "2021-05-15"
-    },
-    {
-        "id": 4,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=24",
-        "author": {
-            "name": "Luca Formicola",
-            "image": null
-        },
-        "likes": 56,
-        "created": "2021-04-03"
-    },
-    {
-        "id": 5,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=534",
-        "author": {
-            "name": "Alessandro Sainato",
-            "image": "https://unsplash.it/300/300?image=29"
-        },
-        "likes": 95,
-        "created": "2021-03-05"
-    }
-];
-
-const containerHTML = document.getElementById('container');
-
-posts.forEach((element) => {
-    containerHTML.innerHTML += `
-    <div class="post">
-            <div class="post__header">
-                <div class="post-meta">                    
-                    <div class="post-meta__icon">
-                        <img class="profile-pic" src="${element.author.image}" alt="Phil Mangione">                    
-                    </div>
-                    <div class="post-meta__data">
-                        <div class="post-meta__author">${element.author.name}</div>
-                        <div class="post-meta__time">${element.created}</div>
-                    </div>                    
-                </div>
-            </div>
-            <div class="post__text">${element.content}</div>
-            <div class="post__image">
-                <img src="${element.media}" alt="">
-            </div>
-            <div class="post__footer">
-                <div class="likes js-likes">
-                    <div class="likes__cta">
-                        <a class="like-button js-like-button" href="#" data-postid="${element.id}">
-                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                            <span class="like-button__label">Mi Piace</span>
-                        </a>
-                    </div>
-                    <div class="likes__counter">
-                        Piace a <b id="like-counter-${element.id}" class="js-likes-counter">${element.likes}</b> persone
-                    </div>
-                </div> 
-            </div>            
-        </div>
-    `
-})
-
-const likeBtn = document.querySelectorAll('.js-like-button');
-
-const likedPosts = [];
-
-likeBtn.forEach(element => {
-    element.addEventListener('click', function() {
-
-        event.preventDefault();
-
-        const postID = Number(this.dataset.postid);
-        const post = posts.find(post => post.id === postID);
-        
-        const isLiked = !post.liked
-        post.liked = isLiked
-
-        const likeIcon = this.querySelector('.like-button__icon');
-        const likeText = this.querySelector('.like-button__label');
-        const likeCounter = document.getElementById(`like-counter-${postID}`)
-
-        if (isLiked) {
-            likeIcon.classList.add('like-button--liked')
-            likeText.classList.add('like-button--liked')
-            post.likes++
-            likedPosts.push(postID)
-            console.log(likedPosts)
-        } else {
-            likeIcon.classList.remove('like-button--liked')
-            likeText.classList.remove('like-button--liked')
-            post.likes--
-            console.log(likedPosts)
+            posts[index].likes--
+            counterLikes[index].innerHTML = posts[index].likes
         }
-        likeCounter.textContent = post.likes
-    });
-});
-            console.log(likedPosts)
-        }
-        likeCounter.textContent = post.likes
     });
 });
